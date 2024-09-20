da8b2da5671cb481a6b5bb925e26259a5f74af49
Adiba khan <140267295+adibakhan432@users.noreply.github.com>
2024-09-20 18:01:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var tabLinks = document.getElementsByClassName("tab-links"),
+    tabContents = document.getElementsByClassName("tab-contents");
+
+function openTab(tabname) {
+    for(item of tabLinks) {
+        item.classList.remove("active-link");
+    }
+    for(item of tabContents) {
+        item.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
